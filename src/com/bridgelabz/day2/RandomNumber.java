54b27bdac0cdeb91bd06744d26c8bd3eb154995c
Atul Gaikar <atulgaikarag9@gmail.com>
2021-09-15 21:09:36 +0530

    Day2 Programs

---
+++ src/com/bridgelabz/day2/RandomNumber.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.day2;
+
+public class RandomNumber {
+	public static void main(String[] args) {
+		int x = (int) ((Math.random() * 6) + 1);
+		System.out.println(x);
+		int y = (int) ((Math.random() * 6) + 1);
+		System.out.println(y);
+		int z = x + y;
+		System.out.println(z);
+           System.out.println(z);
+
+
+	}
+}
